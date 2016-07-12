3fad2699ae4e3b75fb16416064914c9648c69f68
SueBae <suebae26@gmail.com>
2016-07-12 17:52:37 -0400

    2

--- jenkins_test.sh
+++ jenkins_test.sh
@@ -1,2 +1,2 @@
 istanbul cover node_modules/.bin/_mocha -- -r server.js -R tap app/tests/* > test.tap && istanbul report clover
-eslint -c eslint.conf -f checkstyle app/**/*.js > checkstyle-result.xml
+./node_modules/.bin/eslint -c eslint.conf -f checkstyle app/**/*.js > checkstyle-result.xml
