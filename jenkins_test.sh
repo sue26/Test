c76f48513b53829f03302cfa934bd1743599c9f6
SueBae <suebae26@gmail.com>
2016-07-12 19:57:01 -0400

    3

--- jenkins_test.sh
+++ jenkins_test.sh
@@ -1,11 +1,10 @@
 echo '<<<EXECUTING ISTANBUL>>>'
 istanbul cover node_modules/.bin/_mocha -- -r server.js -R tap app/tests/* > test.tap && istanbul report clover
 
-echo '<<<CHANGE INDEX.JS TO PREVENT ERROR>>>'
-cat indexJS > node_modules/eslint/node_modules/strip-bom/index.js
-cat node_modules/eslint/node_modules/strip-bom/index.js
+#echo '<<<CHANGE INDEX.JS TO PREVENT ERROR>>>'
+#cat indexJS > node_modules/eslint/node_modules/strip-bom/index.js
+#cat node_modules/eslint/node_modules/strip-bom/index.js
 
-echo
 echo '<<<EXECUTING ESLINT>>>'
 eslint -c .eslintrc.js -f checkstyle app/**/*.js > checkstyle-result.xml
 
