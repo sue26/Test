7bfe285c24de4cfe00b0b6825af4960713d100df
SueBae <suebae26@gmail.com>
2016-07-12 17:35:20 -0400

    2

--- jenkins_test.sh
+++ jenkins_test.sh
@@ -1,4 +1,2 @@
-export PATH=/usr/local/bin:./node_modules/istanbul:./node_modules/eslint:$PATH;
-
 istanbul cover node_modules/.bin/_mocha -- -r server.js -R tap app/tests/* > test.tap && istanbul report clover
 eslint -f checkstyle app/**/*.js > checkstyle-result.xml
