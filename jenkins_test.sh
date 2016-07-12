2452d4a01d9fe503926749e1fb1b94864f44917a
SueBae <suebae26@gmail.com>
2016-07-12 19:54:11 -0400

    3

--- jenkins_test.sh
+++ jenkins_test.sh
@@ -3,6 +3,9 @@ istanbul cover node_modules/.bin/_mocha -- -r server.js -R tap app/tests/* > tes
 
 echo '<<<CHANGE INDEX.JS TO PREVENT ERROR>>>'
 cat indexJS > node_modules/eslint/node_modules/strip-bom/index.js
+cat node_modules/eslint/node_modules/strip-bom/index.js
+
+echo
 echo '<<<EXECUTING ESLINT>>>'
 eslint -c .eslintrc.js -f checkstyle app/**/*.js > checkstyle-result.xml
 
