f0f09db33160806b337144725d302450e075f08f
SueBae <suebae26@gmail.com>
2016-07-12 20:07:10 -0400

    4

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,8 @@
 module.exports = {
     "env": {
-        "browser": true,
-        "es6": true
+        "browser": true
     },
     "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
-    },
     "rules": {
         "indent": [
             "error",
