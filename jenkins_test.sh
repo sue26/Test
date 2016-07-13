9ebc96cbc7d4fee718d72fd86788dd5eaab065a5
SueBae <suebae26@gmail.com>
2016-07-12 20:34:16 -0400

    4

--- jenkins_test.sh
+++ jenkins_test.sh
@@ -2,5 +2,5 @@ echo '<<<EXECUTING ISTANBUL>>>'
 istanbul cover node_modules/.bin/_mocha -- -r server.js -R tap app/tests/* > test.tap && istanbul report clover
 
 echo '<<<EXECUTING ESLINT>>>'
-eslint -c .eslintrc.js -f checkstyle app/**/*.js > checkstyle-result.xml
+eslint -c .eslintrc.js -f checkstyle app/**/*.js > checkstyle-result.xml || echo 'LINTING FAILED... continuing...'
 
